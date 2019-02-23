c7e9a8a0bfae3c969566b9021e1e278e954c57e2
Adam Cooper <amcooper@gmail.com>
2019-02-23 15:41:43 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::env;
 use std::process;
-use std::fs;
-use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,28 +19,3 @@ fn main() {
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
