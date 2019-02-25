a361e20c4d27fae8adb98818d20b6943dab3f185
Adam Cooper <amcooper@gmail.com>
2019-02-25 08:28:59 -0500

    God which version of Rust am I on? Anyway, fixed.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+extern crate minigrep;
+
 use std::env;
 use std::process;
+
+// use minigrep;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,7 +17,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
