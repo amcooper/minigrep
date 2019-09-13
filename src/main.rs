12b12e5853f9739da40ed724960c5c89092eee70
Adam Cooper <amcooper@gmail.com>
2019-09-13 12:37:47 -0400

    12.6 Printing to stderr

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem processing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
